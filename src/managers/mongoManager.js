f03e14e5dd7d606fe912e4837fbdecdaec202444
Lucas Alves <lucashalves@live.com>
2017-01-11 22:58:49 -0200

    MongoManager updated to use promise.

--- src/managers/mongoManager.js
+++ src/managers/mongoManager.js
@@ -1,6 +1,17 @@
-// mongoose.connect(`mongodb://${user}:pass@localhost:port/database`, {
-const mongoose = require('mongoose');
-const db = mongoose.createConnection();
-db.open('localhost', 'iguess');
+'use strict';
 
-module.exports = () => db
+const mongoose = require('mongoose');
+const promise = require('bluebird');
+
+module.exports = (app) => {
+  const mongo = app.src.config.mongo;
+  const uri = `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`;
+  const options = { promiseLibrary: promise };
+  const db = mongoose.createConnection(uri, options);
+
+  db.on('open', () => {
+    console.log('Mongo Running')
+  });
+
+  return db;
+}
