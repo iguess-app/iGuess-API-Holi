9f62f672220523c0a7c05705ca8665087aeba446
Lucas Alves <lucashalves@live.com>
2017-04-15 17:14:46 -0300

    Controller logic to 'createLeague'

--- src/application/controllers/guessController.js
+++ src/application/controllers/guessController.js
@@ -5,13 +5,25 @@ module.exports = (app) => {
   const guessService = app.src.application.services.guessService;
 
   const laucher = (request, reply) => {
-    
     //Just calling yoo() temporally 
-    const guessesInterpreter = app.src.cron.guessesInterpreter.yoo();
+    //const guessesInterpreter = app.src.cron.guessesInterpreter.yoo();
+  }
 
+  const createLeague = (request, reply) => {
+    const payload = request.payload;
+    const headers = request.headers;
+
+    guessService.createLeague(payload, headers)
+      .then((response) =>
+        reply(response)
+      )
+      .catch((err) =>
+        reply(err)
+      )
   }
 
   return {
-    laucher
+    laucher,
+    createLeague
   }
 }
