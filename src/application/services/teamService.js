ee4e4c9894e86179bfc7bc1962b4a12421dd10e0
Lucas Alves <lucashalves@live.com>
2017-04-23 20:47:13 -0300

    Deleting everything about guess

--- src/application/services/teamService.js
+++
@@ -1,9 +0,0 @@
-'use Strict';
-
-module.exports = (app) => {
-  const teamRepository = app.src.application.repositories.teamRepository;
-
-  const getTeams = (payload) => teamRepository.getTeams(payload)
-  
-  return { getTeams }
-};
