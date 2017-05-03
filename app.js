164ee24d0220dd01fe19ad599bbbab759ba180b9
Lucas Alves <lucashalves@live.com>
2017-05-03 00:16:57 -0300

    Optimizing the app.js and the packageJson to attend the gcloud deploy with no barrier.

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+'use strict';
+
 const consign = require('consign');
 const app = {};
-app.coincidents = require('../IGuess-API-Coincidents/app');
+app.coincidents = require('./IGuess-API-Coincidents/app');
 
 consign()
   .include('configServer.js')
-  .include('src/cron')
   .include('src/repositories')
   .include('src/services')
   .include('src/controllers')
   .include('src/routes')
-  .include('test/unitTests')
-  .include('test/integratedTests')
   .into(app);
 
 app.configServer.start((err) => {
