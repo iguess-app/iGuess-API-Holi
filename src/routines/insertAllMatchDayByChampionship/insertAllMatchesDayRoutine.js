4757cc252ad0428272db28b3f68a78e3ea7f44f9
Lucas Alves <lucashalves@live.com>
2017-11-26 17:20:13 -0200

    Update routine (Added starter log)

--- src/routines/insertAllMatchDayByChampionship/insertAllMatchesDayRoutine.js
+++ src/routines/insertAllMatchDayByChampionship/insertAllMatchesDayRoutine.js
@@ -1,11 +1,14 @@
 'use strict'
 
+const log = require('iguess-api-coincidents').Managers.logManager
+
 const updateAllFixtureService = require('../../services/fixtures/updateAllFixtureService')
 
 const ONE_SECOND = 1000
 const FOUR_HOURS = ONE_SECOND * 60 * 60 * 4
 
 const _startWorker = () => {
+  log.info('==================> ROUTINE STARTED: update All match Schedule <==================')
   updateAllFixtureService({}, {language: 'en-us'})
 }
 
--- src/routines/updateMatchDayResults/updateMatchDayResultRoutine.js
+++ src/routines/updateMatchDayResults/updateMatchDayResultRoutine.js
@@ -1,13 +1,16 @@
 'use strict'
 
+const log = require('iguess-api-coincidents').Managers.logManager
+
 const updateMatchDayResultService = require('../../services/fixtures/updateMatchDayResultService')
 
 const SEVEN_SECONDS = 7000
-const ONE_HOUR = 1000 * 60 * 60
+const FIFTHTEEN_SECONDS = 15000
 
 const _startMatchDayResultWorker = () => {
+  log.info('==================> ROUTINE STARTED: update Match Day Results <==================')
   //TODO: Criar regra para pegar por campeonato e somente em dias e horarios dos jogos
   updateMatchDayResultService({}, {language: 'en-us'})
 }
 
-setInterval(_startMatchDayResultWorker, SEVEN_SECONDS)
+setInterval(_startMatchDayResultWorker, FIFTHTEEN_SECONDS)
