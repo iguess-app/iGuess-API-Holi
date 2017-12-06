d170411bc4100866261f3c3fb572059f091f2875
Lucas Alves <lucashalves@live.com>
2017-12-05 23:35:01 -0200

    Added Association Rule to leagueModel

--- src/models/leagueModel.js
+++ src/models/leagueModel.js
@@ -1,11 +1,16 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const coincidents = require('iguess-api-coincidents')
 
 const optionsSchemas = require('./optionsSchemas/optionsSchemas')
 const db = require('./connect')
 
 const Schema = mongoose.Schema
+const serverErrors = coincidents.Utils.errorUtils.serverErrors
+const config = coincidents.Config
+
+const _validAssociation = (association) => config.associations.includes(association)
 
 const leagueSchema = new Schema({
   country: {
@@ -22,6 +27,10 @@ const leagueSchema = new Schema({
   },
   serie: {
     type: Number
+  },
+  association: {
+    type: String,
+    validate: [_validAssociation, String(serverErrors.notAssociationValid)]    
   }
 }, optionsSchemas.versionKeyDisable)
 
