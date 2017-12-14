901171369b150333695c7b33776d6dd2faae98d9
Lucas Alves <lucashalves@live.com>
2017-12-13 23:31:03 -0200

    Ops and mongo connect

--- src/models/connect.js
+++ src/models/connect.js
@@ -3,7 +3,11 @@
 const coincidents = require('iguess-api-coincidents')
 
 const Managers = coincidents.Managers
+const mongo = coincidents.Config.mongo
+const HOLI_DB_URI = `mongodb://${mongo.host}:${mongo.port}/${mongo.holiDB}`
+let address = ''
 
-const db = Managers.mongoManager()
+mongo.holiAddress ? address = mongo.holiAddress : address = HOLI_DB_URI
+const db = Managers.mongoManager(address)
 
 module.exports = db
