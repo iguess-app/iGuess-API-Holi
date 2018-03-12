155d2fe99fa66270e9062b93e11a8f7dce2e69c4
Lucas Alves <lucashalves@live.com>
2018-03-11 23:28:22 -0300

    Refactoring at apiFootball Parser

--- src/parsers/apiFootballGetEventsParser.js
+++ src/parsers/apiFootballGetEventsParser.js
@@ -5,16 +5,14 @@ const moment = require('moment')
 const apiFootballGetEventsParser = (matchesEvents) =>
   matchesEvents.map((match) => {
     const matchObj = {
-      initTime: moment(`${match.match_date}${match.match_time}Z`, 'YYYY-MM-DDHH:mmZ').utc().format(),
-      ended: match.match_status === 'FT',
+      initTime: _parseToIsoDateUTCFormat(match),
+      ended: _getIfEnded(match),
       homeTeam: match.homeTeamObj,
       awayTeam: match.awayTeamObj,
       started: _getIfStarted(match)
     }
 
-    if (matchObj.started && !matchObj.ended) {
-      matchObj.minutes = match.match_status
-    }
+    _matchIsLive(matchObj) ? matchObj.minutes = match.match_status : ''
 
     if (match.match_hometeam_score !== '' && match.match_hometeam_score !== '?') {
       matchObj.homeTeamScore = match.match_hometeam_score
@@ -24,6 +22,12 @@ const apiFootballGetEventsParser = (matchesEvents) =>
     return matchObj
   })
 
+const _parseToIsoDateUTCFormat = (match) => moment(`${match.match_date}${match.match_time}Z`, 'YYYY-MM-DDHH:mmZ').utc().format()
+
 const _getIfStarted = (match) => match.match_status !== ''
+
+const _getIfEnded = (match) => match.match_status === 'FT'
+
+const _matchIsLive = (matchObj) => matchObj.started && !matchObj.ended
 
 module.exports = apiFootballGetEventsParser
