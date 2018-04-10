d078c720c00f331b37ea26255cec360d1abb5bd0
Lucas Alves <lucashalves@live.com>
2018-04-09 22:14:35 -0300

    Added function to update data at fisrt routine

--- src/repositories/fixtures/insertNewMatchDayAtRoundsRepository.js
+++ src/repositories/fixtures/insertNewMatchDayAtRoundsRepository.js
@@ -15,26 +15,39 @@ const insertNewMatchDayAtRoundsRepository = (leagueObj, matchesEvents) =>
     }
 
     return Match.findOne(searchQuery)
-      .then((roundFound) => {
-        if (!roundFound) {
+      .then((matchFound) => {
+        if (!matchFound) {
           Match.create(_buildNewMatchObj(match, leagueObj))
             .catch((err) => log.error(err))
         }
         log.info(`Match already setted (championshipRef: ${leagueObj.currentChampionshipRef}, ${match.homeTeam.shortName} X  ${match.awayTeam.shortName} [${match.initTime}])`)
+        _updateMatchDataIfNeeds(matchFound, match)
       })
   }
   )
 
 const _buildNewMatchObj = (match, leagueObj) => ({
-  'matchRef': sharedFunctions.buildMatchRef(match, leagueObj),
-  'championshipRef': leagueObj.currentChampionshipRef,
-  'initTime': match.initTime,
-  'homeTeam': match.homeTeam,
-  'awayTeam': match.awayTeam,
-  'homeTeamScore': match.homeTeamScore,
-  'awayTeamScore': match.awayTeamScore,
-  'started': match.started,
-  'ended': match.ended
+  matchRef: sharedFunctions.buildMatchRef(match, leagueObj),
+  championshipRef: leagueObj.currentChampionshipRef,
+  initTime: match.initTime,
+  homeTeam: match.homeTeam,
+  awayTeam: match.awayTeam,
+  homeTeamScore: match.homeTeamScore,
+  awayTeamScore: match.awayTeamScore,
+  started: match.started,
+  ended: match.ended
 })
+
+const _updateMatchDataIfNeeds = (matchFound, match) => {
+  matchFound.homeTeamScore = match.homeTeamScore
+  matchFound.awayTeamScore = match.awayTeamScore
+  matchFound.ended = match.ended
+  matchFound.started = match.started
+  matchFound.initTime = match.initTime
+  if (match.minutes) {
+    matchFound.minutes = match.minutes
+  }
+  matchFound.save()
+}
 
 module.exports = insertNewMatchDayAtRoundsRepository
